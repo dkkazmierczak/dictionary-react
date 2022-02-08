f12bdc32b44684ea0a9177126539c552f39541f0
Dominika Kaźmierczak <dkl.kazmierczak@gmail.com>
2022-02-08 23:00:06 +0100

    Added functions handling searching engine

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,18 +1,33 @@
+import { useState } from "react"
 import "./Search.scss"
 
 const Search = () => {
+  const [search, setSearch] = useState("")
+  const [change, setChange] = useState("")
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    setSearch(change)
+  }
+
+  const handleChange = event => {
+    setChange(event.target.value)
+  }
+
   return (
     <div className="Search">
       <h1>Search</h1>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           type="search"
           placeholder=" search..."
           autoFocus={true}
           className="search-input"
+          onChange={handleChange}
         />
         <input type="submit" value="search" />
       </form>
+      <h2>{search}</h2>
     </div>
   )
 }
